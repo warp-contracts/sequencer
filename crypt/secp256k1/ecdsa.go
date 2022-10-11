031e980ee22912c195f6f424ddd185b0c37c6585
Max Levitskiy <max.dstu@gmail.com>
2022-10-11 10:16:08 +0200

    Copy secp256k1 from go-dappley

---
+++ crypt/secp256k1/ecdsa.go
@@ -0,0 +1,143 @@
+// Copyright (C) 2017 go-nebulas authors
+//
+// This file is part of the go-nebulas library.
+//
+// the go-nebulas library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// the go-nebulas library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the go-nebulas library.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// use btcec https://godoc.org/github.com/btcsuite/btcd/btcec#example-package--VerifySignature
+
+package secp256k1
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+
+	"encoding/hex"
+	"errors"
+	"math/big"
+
+	"crypto/rand"
+
+	"github.com/dappley/go-dappley/crypto/keystore/secp256k1/bitelliptic"
+)
+
+// S256 returns an instance of the secp256k1 curve.
+func S256() elliptic.Curve {
+	return bitelliptic.S256()
+}
+
+// NewECDSAPrivateKey generate a ecdsa private key
+func NewECDSAPrivateKey() (*ecdsa.PrivateKey, error) {
+	var priv *ecdsa.PrivateKey
+
+	// in bitcoin src, they call SeckeyVerify func to verify the generated private key
+	// to make sure valid.
+	for {
+		privKey, err := ecdsa.GenerateKey(S256(), rand.Reader)
+		if err != nil {
+			return nil, err
+		}
+		privData, err := FromECDSAPrivateKey(privKey)
+		if err != nil {
+			return nil, err
+		}
+		priv = privKey
+		if SeckeyVerify(privData) {
+			break
+		}
+	}
+	return priv, nil
+}
+
+// FromECDSAPrivateKey exports a private key into a binary dump.
+func FromECDSAPrivateKey(priv *ecdsa.PrivateKey) ([]byte, error) {
+	if priv == nil {
+		return nil, errors.New("ecdsa: please input private key")
+	}
+	// as private key len cannot guarantee greater than Params bytes len, padding big bytes.
+	return paddedBigBytes(priv.D, priv.Params().BitSize/8), nil
+}
+
+// FromECDSAPublicKey exports a public key into a binary dump.
+func FromECDSAPublicKey(pub *ecdsa.PublicKey) ([]byte, error) {
+	if pub == nil || pub.X == nil || pub.Y == nil {
+		return nil, errors.New("ecdsa: please input public key")
+	}
+	return elliptic.Marshal(S256(), pub.X, pub.Y), nil
+}
+
+// HexToECDSAPrivateKey parses a secp256k1 private key.
+func HexToECDSAPrivateKey(hexkey string) (*ecdsa.PrivateKey, error) {
+	b, err := hex.DecodeString(hexkey)
+	if err != nil {
+		return nil, err
+	}
+	return ToECDSAPrivateKey(b)
+}
+
+// ToECDSAPrivateKey creates a private key with the given data value.
+func ToECDSAPrivateKey(d []byte) (*ecdsa.PrivateKey, error) {
+	priv := new(ecdsa.PrivateKey)
+	priv.PublicKey.Curve = S256()
+	priv.D = new(big.Int).SetBytes(d)
+	priv.PublicKey.X, priv.PublicKey.Y = priv.PublicKey.Curve.ScalarBaseMult(d)
+	return priv, nil
+}
+
+// ToECDSAPublicKey creates a public key with the given data value.
+func ToECDSAPublicKey(pub []byte) (*ecdsa.PublicKey, error) {
+	if len(pub) == 0 {
+		return nil, errors.New("ecdsa: please input public key bytes")
+	}
+	x, y := elliptic.Unmarshal(S256(), pub)
+	return &ecdsa.PublicKey{Curve: S256(), X: x, Y: y}, nil
+}
+
+// zeroKey zeroes the private key
+func zeroKey(k *ecdsa.PrivateKey) {
+	b := k.D.Bits()
+	for i := range b {
+		b[i] = 0
+	}
+}
+
+// paddedBigBytes encodes a big integer as a big-endian byte slice.
+func paddedBigBytes(bigint *big.Int, n int) []byte {
+	if bigint.BitLen()/8 >= n {
+		return bigint.Bytes()
+	}
+	ret := make([]byte, n)
+	readBits(bigint, ret)
+	return ret
+}
+
+const (
+	// number of bits in a big.Word
+	wordBits = 32 << (uint64(^big.Word(0)) >> 63)
+	// number of bytes in a big.Word
+	wordBytes = wordBits / 8
+)
+
+// readBits encodes the absolute value of bigint as big-endian bytes.
+func readBits(bigint *big.Int, buf []byte) {
+	i := len(buf)
+	for _, d := range bigint.Bits() {
+		for j := 0; j < wordBytes && i > 0; j++ {
+			i--
+			buf[i] = byte(d)
+			d >>= 8
+		}
+	}
+}
---
+++ crypt/secp256k1/secp256k1.go
@@ -0,0 +1,205 @@
+// Copyright (C) 2017 go-nebulas authors
+//
+// This file is part of the go-nebulas library.
+//
+// the go-nebulas library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// the go-nebulas library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the go-nebulas library.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+package secp256k1
+
+/*
+#cgo CFLAGS: -I./libsecp256k1
+#cgo CFLAGS: -I./libsecp256k1/src/
+#define USE_NUM_NONE
+#define USE_FIELD_10X26
+#define USE_FIELD_INV_BUILTIN
+#define USE_SCALAR_8X32
+#define USE_SCALAR_INV_BUILTIN
+#define ENABLE_MODULE_RECOVERY
+#define NDEBUG
+#include "./libsecp256k1/src/secp256k1.c"
+*/
+import "C"
+
+//#cgo CFLAGS: -std=gnu99 -Wno-error
+//#cgo LDFLAGS: -lgmp
+//#cgo CFLAGS: -Wno-error
+
+import (
+	"errors"
+	"unsafe"
+
+	"github.com/dappley/go-dappley/crypto/utils"
+)
+
+const (
+	// EcdsaPrivateKeyLength private key length
+	EcdsaPrivateKeyLength = 32
+)
+
+var (
+	// ErrInvalidMsgLen invalid message length
+	ErrInvalidMsgLen = errors.New("invalid message length, need 32 bytes")
+
+	// ErrGetPublicKeyFailed private key to public failed
+	ErrGetPublicKeyFailed = errors.New("private key to public failed")
+
+	// ErrInvalidSignature invalid signature length
+	ErrInvalidSignature = errors.New("invalid signature")
+
+	// ErrInvalidPrivateKey invalid private key
+	ErrInvalidPrivateKey = errors.New("invalid private key")
+
+	// ErrInvalidPublicKey invalid public key
+	ErrInvalidPublicKey = errors.New("invalid public key")
+
+	// ErrSignFailed sign failed
+	ErrSignFailed = errors.New("sign failed")
+
+	// ErrRecoverFailed recover failed
+	ErrRecoverFailed = errors.New("recovery failed")
+)
+
+var ctx *C.secp256k1_context
+
+// use bitcoin's libsecp256k1 library
+// use like https://github.com/btccom/secp256k1-go/blob/master/secp256k1/secp256k1.go
+
+func init() {
+	ctx = C.secp256k1_context_create(C.SECP256K1_CONTEXT_SIGN | C.SECP256K1_CONTEXT_VERIFY)
+}
+
+// NewSeckey generate a ecdsa private key by secp256k1
+func NewSeckey() []byte {
+	var priv []byte
+
+	// in bitcoin src, they call SeckeyVerify func to verify the generated private key
+	// to make sure valid.
+	for {
+		priv = utils.RandomCSPRNG(EcdsaPrivateKeyLength)
+		if SeckeyVerify(priv) {
+			break
+		}
+	}
+	return priv
+}
+
+// SeckeyVerify check private is ok for secp256k1
+func SeckeyVerify(seckey []byte) bool {
+	return C.secp256k1_ec_seckey_verify(ctx, cBuf(seckey)) == 1
+}
+
+// GetPublicKey private key to public key
+func GetPublicKey(seckey []byte) ([]byte, error) {
+
+	var pubkey C.secp256k1_pubkey
+	result := int(C.secp256k1_ec_pubkey_create(ctx, &pubkey, cBuf(seckey)))
+	if result != 1 {
+		return nil, ErrGetPublicKeyFailed
+	}
+
+	output := make([]C.uchar, 65)
+	outputLen := C.size_t(65)
+	result = int(C.secp256k1_ec_pubkey_serialize(ctx, &output[0], &outputLen, &pubkey, C.SECP256K1_EC_UNCOMPRESSED))
+	if result != 1 {
+		return nil, ErrGetPublicKeyFailed
+	}
+	return goBytes(output, C.int(outputLen)), nil
+}
+
+// RecoverECDSAPublicKey recover verifies the compact signature "signature" of "hash"
+func RecoverECDSAPublicKey(msg []byte, signature []byte) ([]byte, error) {
+	if len(msg) != 32 {
+		return nil, ErrInvalidMsgLen
+	}
+	if len(signature) != 65 {
+		return nil, ErrInvalidSignature
+	}
+	var (
+		sig    C.secp256k1_ecdsa_recoverable_signature
+		pubkey C.secp256k1_pubkey
+	)
+
+	result := int(C.secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &sig, (*C.uchar)(unsafe.Pointer(&signature[0])), (C.int(signature[64]))))
+	if result != 1 {
+		return nil, ErrRecoverFailed
+	}
+	if int(C.secp256k1_ecdsa_recover(ctx, &pubkey, &sig, cBuf(msg))) != 1 {
+		return nil, ErrRecoverFailed
+	}
+	output := make([]C.uchar, 65)
+	outputLen := C.size_t(65)
+	result = int(C.secp256k1_ec_pubkey_serialize(ctx, &output[0], &outputLen, &pubkey, C.SECP256K1_EC_UNCOMPRESSED))
+	if result != 1 {
+		return nil, ErrRecoverFailed
+	}
+	return goBytes(output, C.int(outputLen)), nil
+}
+
+// Sign sign hash with private key
+func Sign(msg []byte, seckey []byte) ([]byte, error) {
+	if len(msg) != 32 {
+		return nil, ErrInvalidMsgLen
+	}
+
+	if C.secp256k1_ec_seckey_verify(ctx, cBuf(seckey)) != 1 {
+		return nil, ErrInvalidPrivateKey
+	}
+
+	var (
+		noncefunc = C.secp256k1_nonce_function_rfc6979
+		sigstruct C.secp256k1_ecdsa_recoverable_signature
+	)
+	if C.secp256k1_ecdsa_sign_recoverable(ctx, &sigstruct, cBuf(msg), cBuf(seckey), noncefunc, nil) == 0 {
+		return nil, ErrSignFailed
+	}
+
+	var (
+		sig   = make([]byte, 65)
+		recid C.int
+	)
+	C.secp256k1_ecdsa_recoverable_signature_serialize_compact(ctx, cBuf(sig), &recid, &sigstruct)
+	sig[64] = byte(recid) // add back recid to get 65 bytes sig
+	return sig, nil
+}
+
+// Verify verify with public key
+func Verify(msg []byte, signature []byte, pub []byte) (bool, error) {
+	if len(msg) != 32 {
+		return false, ErrInvalidMsgLen
+	}
+
+	var (
+		sig    C.secp256k1_ecdsa_signature
+		pubkey C.secp256k1_pubkey
+	)
+	result := int(C.secp256k1_ec_pubkey_parse(ctx, &pubkey, cBuf(pub), C.size_t(len(pub))))
+	if result != 1 {
+		return false, ErrInvalidPublicKey
+	}
+	result = int(C.secp256k1_ecdsa_signature_parse_compact(ctx, &sig, cBuf(signature)))
+	if result != 1 {
+		return false, ErrInvalidSignature
+	}
+	result = int(C.secp256k1_ecdsa_verify(ctx, &sig, cBuf(msg), &pubkey))
+	return result == 1, nil
+}
+
+func cBuf(goSlice []byte) *C.uchar {
+	return (*C.uchar)(unsafe.Pointer(&goSlice[0]))
+}
+
+func goBytes(cSlice []C.uchar, size C.int) []byte {
+	return C.GoBytes(unsafe.Pointer(&cSlice[0]), size)
+}
