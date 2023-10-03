141190751064671ac58164e8eae33d3dfdc20434
Michał Szynwelski <szynek@warp.cc>
2023-10-03 16:39:06 +0200

    Calculating a random value for each interaction

--- x/sequencer/proposal/prepare.go
+++ x/sequencer/proposal/prepare.go
@@ -15,54 +15,64 @@ import (
 	"github.com/warp-contracts/sequencer/x/sequencer/types"
 )
 
-// Sets sort keys for all L2 interactions and adds an Arweave block transaction to the beginning of the block if needed.
+type prepareProposalHandler struct {
+	keeper            *keeper.Keeper
+	arweaveController controller.ArweaveBlocksController
+	txConfig          client.TxConfig
+}
+
 func NewPrepareProposalHandler(keeper *keeper.Keeper, arweaveController controller.ArweaveBlocksController, txConfig client.TxConfig) sdk.PrepareProposalHandler {
-	return func(ctx sdk.Context, req abci.RequestPrepareProposal) abci.ResponsePrepareProposal {
-		lastBlock := keeper.MustGetLastArweaveBlock(ctx)
-		arweaveHeight := lastBlock.ArweaveBlock.Height
-		sequencerHeight := ctx.BlockHeader().Height
-		nextBlock := arweaveController.GetNextArweaveBlock(arweaveHeight + 1)
-		lastSortKeys := newLastSortKeys(keeper, ctx)
-		arweaveBlockTx, i := createArweaveTx(ctx, txConfig, nextBlock, lastSortKeys)
-		sortKey := newSortKey(arweaveHeight+uint64(i), sequencerHeight)
-		var size int64 = 0
+	handler := &prepareProposalHandler{keeper, arweaveController, txConfig}
+	return handler.prepare
+}
 
-		result := make([][]byte, len(req.Txs)+i)
-		if arweaveBlockTx != nil {
-			result[0] = arweaveBlockTx
-			size += protoTxSize(arweaveBlockTx)
-			if size > req.MaxTxBytes {
-				panic(fmt.Sprintf("MaxTxBytes limit (%d) is too small! It is smaller than the size of the Arweave block (%d)",
-					req.MaxTxBytes, size))
-			}
+// Sets sort keys and random value for all L2 interactions and adds an Arweave block transaction to the beginning of the block if needed.
+func (h *prepareProposalHandler) prepare(ctx sdk.Context, req abci.RequestPrepareProposal) abci.ResponsePrepareProposal {
+	lastBlock := h.keeper.MustGetLastArweaveBlock(ctx)
+	arweaveHeight := lastBlock.ArweaveBlock.Height
+	sequencerHeight := ctx.BlockHeader().Height
+	sequencerBlockHash := ctx.BlockHeader().LastBlockId.Hash
+	nextBlock := h.arweaveController.GetNextArweaveBlock(arweaveHeight + 1)
+	lastSortKeys := newLastSortKeys(h.keeper, ctx)
+	arweaveBlockTx, i := h.createArweaveTx(ctx, nextBlock, lastSortKeys)
+	sortKey := newSortKey(arweaveHeight+uint64(i), sequencerHeight)
+	var size int64 = 0
+
+	result := make([][]byte, len(req.Txs)+i)
+	if arweaveBlockTx != nil {
+		result[0] = arweaveBlockTx
+		size += protoTxSize(arweaveBlockTx)
+		if size > req.MaxTxBytes {
+			panic(fmt.Sprintf("MaxTxBytes limit (%d) is too small! It is smaller than the size of the Arweave block (%d)",
+				req.MaxTxBytes, size))
 		}
-
-		txCount := i
-		for txCount < len(req.Txs)+i {
-			txBytes := setSortKeysInDataItem(txConfig, req.Txs[txCount-i], sortKey, lastSortKeys)
-			txSize := protoTxSize(txBytes)
-			if size+txSize > req.MaxTxBytes {
-				break
-			}
-			result[txCount] = txBytes
-			txCount++
-			size += txSize
-		}
-
-		ctx.Logger().
-			With("height", req.Height).
-			With("number of txs", txCount).
-			With("size of txs", size).
-			With("max size", req.MaxTxBytes).
-			Info("Prepared transactions")
-
-		return abci.ResponsePrepareProposal{Txs: result[:txCount]}
 	}
+
+	txCount := i
+	for txCount < len(req.Txs)+i {
+		txBytes := h.prepareDataItem(sequencerBlockHash, req.Txs[txCount-i], sortKey, lastSortKeys)
+		txSize := protoTxSize(txBytes)
+		if size+txSize > req.MaxTxBytes {
+			break
+		}
+		result[txCount] = txBytes
+		txCount++
+		size += txSize
+	}
+
+	ctx.Logger().
+		With("height", req.Height).
+		With("number of txs", txCount).
+		With("size of txs", size).
+		With("max size", req.MaxTxBytes).
+		Info("Prepared transactions")
+
+	return abci.ResponsePrepareProposal{Txs: result[:txCount]}
 }
 
 // Returns the transaction with an Arweave block if it is older than an hour and has not been added to the blockchain yet.
 // Additionally, it returns 1 if such a block exists and 0 otherwise.
-func createArweaveTx(ctx sdk.Context, txConfig client.TxConfig, nextArweaveBlock *types.NextArweaveBlock, lastSortKeys *LastSortKeys) ([]byte, int) {
+func (h *prepareProposalHandler) createArweaveTx(ctx sdk.Context, nextArweaveBlock *types.NextArweaveBlock, lastSortKeys *LastSortKeys) ([]byte, int) {
 	if nextArweaveBlock == nil || !types.IsArweaveBlockOldEnough(ctx, nextArweaveBlock.BlockInfo) {
 		return nil, 0
 	}
@@ -72,37 +82,38 @@ func createArweaveTx(ctx sdk.Context, txConfig client.TxConfig, nextArweaveBlock
 		Transactions: prepareTransactions(nextArweaveBlock.Transactions, lastSortKeys),
 	}
 
-	txBuilder := txConfig.NewTxBuilder()
+	txBuilder := h.txConfig.NewTxBuilder()
 	err := txBuilder.SetMsgs(msg)
 	if err != nil {
 		panic(err)
 	}
 
 	tx := txBuilder.GetTx()
-	bz, err := txConfig.TxEncoder()(tx)
+	bz, err := h.txConfig.TxEncoder()(tx)
 	if err != nil {
 		panic(err)
 	}
 	return bz, 1
 }
 
-// Sets the LastSortKey values for transactions from the Arweave block
-func prepareTransactions(txs []*types.ArweaveTransaction, lastSortKeys *LastSortKeys) []*types.ArweaveTransactionWithLastSortKey {
-	result := make([]*types.ArweaveTransactionWithLastSortKey, len(txs))
+// Sets the LastSortKey and random values for transactions from the Arweave block
+func prepareTransactions(txs []*types.ArweaveTransaction, lastSortKeys *LastSortKeys) []*types.ArweaveTransactionWithInfo {
+	result := make([]*types.ArweaveTransactionWithInfo, len(txs))
 	for i, tx := range txs {
-		result[i] = &types.ArweaveTransactionWithLastSortKey{
+		result[i] = &types.ArweaveTransactionWithInfo{
 			Transaction: tx,
 			LastSortKey: lastSortKeys.getAndStoreLastSortKey(tx.Contract, tx.SortKey),
+			Random:      generateRandomL1(tx.SortKey),
 		}
 	}
 	return result
 }
 
-// Sets 'SortKey' and 'LastSortKey' if the transaction is an L2 interaction.
+// Sets 'SortKey', 'LastSortKey' and random value if the transaction is an L2 interaction.
 // Returns the original transaction otherwise.
-func setSortKeysInDataItem(txConfig client.TxConfig, txBytes []byte, sortKey *SortKey, lastSortKeys *LastSortKeys) []byte {
+func (h *prepareProposalHandler) prepareDataItem(sequencerBlockHash []byte, txBytes []byte, sortKey *SortKey, lastSortKeys *LastSortKeys) []byte {
 	// decode tx
-	tx, err := txConfig.TxDecoder()(txBytes)
+	tx, err := h.txConfig.TxDecoder()(txBytes)
 	if err != nil {
 		panic(err)
 	}
@@ -121,8 +132,11 @@ func setSortKeysInDataItem(txConfig client.TxConfig, txBytes []byte, sortKey *So
 		}
 		dataItem.LastSortKey = lastSortKeys.getAndStoreLastSortKey(contract, dataItem.SortKey)
 
+		// set random value
+		dataItem.Random = generateRandomL2(sequencerBlockHash, dataItem.SortKey)
+
 		// encode tx
-		txBuilder, err := txConfig.WrapTxBuilder(tx)
+		txBuilder, err := h.txConfig.WrapTxBuilder(tx)
 		if err != nil {
 			panic(err)
 		}
@@ -130,7 +144,7 @@ func setSortKeysInDataItem(txConfig client.TxConfig, txBytes []byte, sortKey *So
 		if err != nil {
 			panic(err)
 		}
-		txBytes, err = txConfig.TxEncoder()(txBuilder.GetTx())
+		txBytes, err = h.txConfig.TxEncoder()(txBuilder.GetTx())
 		if err != nil {
 			panic(err)
 		}
--- x/sequencer/proposal/process_data_item.go
+++ x/sequencer/proposal/process_data_item.go
@@ -1,6 +1,8 @@
 package proposal
 
 import (
+	"bytes"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/warp-contracts/sequencer/x/sequencer/types"
@@ -17,8 +19,8 @@ func getDataItemMsg(tx sdk.Tx) *types.MsgDataItem {
 	return nil
 }
 
-func (h *processProposalHandler) processProposalValidateDataItem(msg *types.MsgDataItem) bool {
-	return h.checkSortKey(msg) && h.checkLastSortKey(msg)
+func (h *processProposalHandler) processProposalValidateDataItem(ctx sdk.Context, msg *types.MsgDataItem) bool {
+	return h.checkSortKey(msg) && h.checkLastSortKey(msg) && h.checkRandom(ctx, msg)
 }
 
 func (h *processProposalHandler) checkSortKey(msg *types.MsgDataItem) bool {
@@ -54,4 +56,13 @@ func (h *processProposalHandler) checkLastSortKey(msg *types.MsgDataItem) bool {
 func (h *processProposalHandler) initSortKeyForBlock(ctx sdk.Context) {
 	h.sortKey = newSortKey(h.keeper.MustGetLastArweaveBlock(ctx).ArweaveBlock.Height, ctx.BlockHeight())
 	h.lastSortKeys = newLastSortKeys(h.keeper, ctx)
+}
+
+func (h *processProposalHandler) checkRandom(ctx sdk.Context, msg *types.MsgDataItem) bool {
+	expectedRandom := generateRandomL2(ctx.BlockHeader().LastBlockId.Hash, msg.SortKey)
+	if !bytes.Equal(msg.Random, expectedRandom) {
+		return h.rejectProposal("invalid random value", "expected", expectedRandom, "actual", msg.Random)
+	}
+
+	return true
 }
