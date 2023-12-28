5ef9debdb8858f015e903a891d082f7d1319fafd
Jan Olszak <jan@warp.cc>
2023-12-28 16:36:12 +0100

    Limiter: Fix linter errors

--- x/limiter/types/errors.go
+++ x/limiter/types/errors.go
@@ -2,11 +2,7 @@ package types
 
 // DONTCOVER
 
-import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-)
-
 // x/limiter module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+// ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
 )
