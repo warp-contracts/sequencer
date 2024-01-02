5c184209e8939d506bebc8b9a3886981dec972be
Jan Olszak <jan@warp.cc>
2024-01-02 16:25:42 +0100

    Limiter: Increment counter when processing L2 interactions

--- x/sequencer/keeper/msg_server_data_item.go
+++ x/sequencer/keeper/msg_server_data_item.go
@@ -10,26 +10,26 @@ import (
 
 func (k *msgServer) DataItem(goCtx context.Context, msg *types.MsgDataItem) (*types.MsgDataItemResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-	if err := k.setContractPrevSortKey(ctx, msg); err != nil {
+
+	// Find the contract id from the tags
+	contract, err := msg.GetContractFromTags()
+	if err != nil {
 		return nil, err
 	}
 
-	k.blockInteractions.Add(msg)
-
-	return &types.MsgDataItemResponse{}, nil
-}
-
-func (k *msgServer) setContractPrevSortKey(ctx sdk.Context, msg *types.MsgDataItem) error {
-	contract, err := msg.GetContractFromTags()
-	if err != nil {
-		return err
-	}
-
+	// Assign prevSortKey
 	prevSortKey := types.PrevSortKey{
 		Contract: contract,
 		SortKey:  msg.SortKey,
 	}
 
 	k.SetPrevSortKey(ctx, prevSortKey)
-	return nil
+
+	// Increment the limiter counter that corresponds to the contract
+	k.limiterKeeper.Inc(ctx, 0, []byte(contract))
+
+	// L2 interactions contained in the last sequencer block
+	k.blockInteractions.Add(msg)
+
+	return &types.MsgDataItemResponse{}, nil
 }
